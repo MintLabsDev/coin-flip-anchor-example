8d225016152552b1a52d8646e5c530860e25aa4f
caglarGokce <93769069+caglarGokce@users.noreply.github.com>
2024-06-26 17:06:17 +0300

    first commit

---
+++ client/client.ts
@@ -0,0 +1,162 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { CoinFlip } from "../target/types/coin_flip";
+import { PublicKey,Keypair } from "@solana/web3.js";
+import { deserialize } from "borsh";
+import bs58 from "bs58";
+
+
+const provider = anchor.AnchorProvider.env()
+anchor.setProvider(provider);
+
+export class CurrentFeed {
+  is_init: number = 0;
+  fee: number = 0;
+  offset1: number = 0;
+  offset2: number = 0;
+  offset3: number = 0;
+  offset4: number = 0;
+  offset5: number = 0;
+  offset6: number = 0;
+  offset7: number = 0;
+  offset8: number = 0;
+  account1: number[] = Array.from({ length: 32 }, () => 1);
+  account2: number[] = Array.from({ length: 32 }, () => 1);
+  account3: number[] = Array.from({ length: 32 }, () => 1);
+  fallback_account: number[] = Array.from({ length: 32 }, () => 1);
+  bump: number = 0;
+
+  constructor(
+    fields:
+      | {
+          is_init: number;
+          fee: number;
+          offset1: number;
+          offset2: number;
+          offset3: number;
+          offset4: number;
+          offset5: number;
+          offset6: number;
+          offset7: number;
+          offset8: number;
+          account1: number[];
+          account2: number[];
+          account3: number[];
+          fallback_account: number[];
+          bump: number;
+        }
+      | undefined = undefined
+  ) {
+    if (fields) {
+      this.is_init = fields.is_init;
+      this.fee = fields.fee;
+      this.offset1 = fields.offset1;
+      this.offset2 = fields.offset2;
+      this.offset3 = fields.offset3;
+      this.offset4 = fields.offset4;
+      this.offset5 = fields.offset5;
+      this.offset6 = fields.offset6;
+      this.offset7 = fields.offset7;
+      this.offset8 = fields.offset8;
+      this.account1 = fields.account1;
+      this.account2 = fields.account2;
+      this.account3 = fields.account3;
+      this.fallback_account = fields.fallback_account;
+      this.bump = fields.bump;
+    }
+  }
+}
+export const CurrentFeedSchema = new Map([
+  [
+    CurrentFeed,
+    {
+      kind: "struct",
+      fields: [
+        ["is_init", "u8"],
+        ["fee", "u64"],
+        ["offset1", "u8"],
+        ["offset2", "u8"],
+        ["offset3", "u8"],
+        ["offset4", "u8"],
+        ["offset5", "u8"],
+        ["offset6", "u8"],
+        ["offset7", "u8"],
+        ["offset8", "u8"],
+        ["account1", ["u8", 32]],
+        ["account2", ["u8", 32]],
+        ["account3", ["u8", 32]],
+        ["fallback_account", ["u8", 32]],
+        ["bump", "u8"],
+      ],
+    },
+  ],
+]);
+
+const rngProgram = new anchor.web3.PublicKey('5fiZruYuczm75PhTWhkoug5xaZUPtLEmzqc2AWYyZDzM');
+
+const program = new Program<CoinFlip>(
+  require("../target/idl/test_rng.json"),
+  provider
+);
+
+async function main(decision:number, player:Keypair) {
+
+
+
+  const connection = program.provider.connection;
+
+
+  const current_feeds_account = PublicKey.findProgramAddressSync(
+    [Buffer.from("c"), Buffer.from([1])],
+    rngProgram
+  );
+  console.log();
+
+  const currentFeedsAccountInfo = await connection.getAccountInfo(
+    current_feeds_account[0]
+  );
+  const currentFeedsAccountData = deserialize(
+    CurrentFeedSchema,
+    CurrentFeed,
+    currentFeedsAccountInfo?.data!
+  );
+
+  const feedAccount1 = new PublicKey(
+    bs58.encode(currentFeedsAccountData.account1).toString()
+  );
+  const feedAccount2 = new PublicKey(
+    bs58.encode(currentFeedsAccountData.account2).toString()
+  );
+  const feedAccount3 = new PublicKey(
+    bs58.encode(currentFeedsAccountData.account3).toString()
+  );
+
+  const fallbackAccount = new PublicKey(
+    bs58.encode(currentFeedsAccountData.fallback_account).toString()
+  );
+
+  const tempKeypair = anchor.web3.Keypair.generate();
+
+  const playersDecision = { decision: new anchor.BN(decision) };
+
+  const tx = await program.methods
+    .getRandom(playersDecision)
+    .accounts({
+      signer: player.publicKey,
+      feedAccount1: feedAccount1,
+      feedAccount2: feedAccount2,
+      feedAccount3: feedAccount3,
+      fallbackAccount: fallbackAccount,
+      temp: tempKeypair.publicKey,
+      rngProgram: rngProgram,
+      currentFeedsAccount: current_feeds_account[0],
+    })
+    .signers([player, tempKeypair])
+    .rpc();
+
+  console.log('Transaction signature:', tx);
+}
+
+
+
+
---
+++ programs/feed-protocol-example-anchor-program/src/lib.rs
@@ -0,0 +1,119 @@
+use anchor_lang::prelude::*;
+use borsh::{BorshDeserialize, BorshSerialize};
+
+declare_id!("5fiZruYuczm75PhTWhkoug5xaZUPtLEmzqc2AWYyZDzM");
+
+#[program]
+mod coin_flip {
+    use anchor_lang::solana_program::{instruction::Instruction, program::{get_return_data, invoke}};
+
+    use super::*;
+
+    pub fn get_random(ctx: Context<GetRand>, players_decision:PlayersDecision) -> Result<()> {
+        let rng_program: &Pubkey = ctx.accounts.rng_program.key;
+   
+        //Creating instruction for cross program invocation to RNG_PROGRAM
+        let instruction: Instruction = Instruction {
+            program_id: *rng_program,
+            accounts: vec![
+                ctx.accounts.signer.to_account_metas(Some(true))[0].clone(),
+                ctx.accounts.feed_account_1.to_account_metas(Some(false))[0].clone(),
+                ctx.accounts.feed_account_2.to_account_metas(Some(false))[0].clone(),
+                ctx.accounts.feed_account_3.to_account_metas(Some(false))[0].clone(),
+                ctx.accounts.fallback_account.to_account_metas(Some(false))[0].clone(),
+                ctx.accounts.current_feeds_account.to_account_metas(Some(false))[0].clone(),
+                ctx.accounts.temp.to_account_metas(Some(true))[0].clone(),
+                ctx.accounts.system_program.to_account_metas(Some(false))[0].clone(),
+            ],
+            data: vec![0],
+        };
+
+        //Creating account infos for CPI to RNG_PROGRAM
+        let account_infos: &[AccountInfo; 8] = &[
+            ctx.accounts.signer.to_account_info().clone(),
+            ctx.accounts.feed_account_1.to_account_info().clone(),
+            ctx.accounts.feed_account_2.to_account_info().clone(),
+            ctx.accounts.feed_account_3.to_account_info().clone(),
+            ctx.accounts.fallback_account.to_account_info().clone(),
+            ctx.accounts.current_feeds_account.to_account_info().clone(),
+            ctx.accounts.temp.to_account_info().clone(),
+            ctx.accounts.system_program.to_account_info().clone(),
+        ];
+
+        //CPI to RNG_PROGRAM
+        invoke(&instruction, account_infos)?;
+
+        let returned_data: (Pubkey, Vec<u8>) = get_return_data().unwrap();
+
+        //Random number is returned from the RNG_PROGRAM
+        let random_number: RandomNumber;
+        if &returned_data.0 == rng_program {
+            random_number = RandomNumber::try_from_slice(&returned_data.1)?;
+            msg!("{}", random_number.random_number);
+
+
+        } else {
+            return Err(ErrorCode::FailedToGetRandomNumber.into());
+        }
+
+        //Checking players input - zero is head, one is tails
+        if players_decision.decision != 0 && players_decision.decision != 1 {
+            return Err(ErrorCode::InvalidDecision.into());
+        }
+        
+        //we get the mod 2 of the random number. It is either one or zero
+        //then we compare with the player's decision just log a message. you can put here your program logic
+        if random_number.random_number % 2 == players_decision.decision{
+            msg!("congragulations you win");
+        }else{
+            msg!("you lost");
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Debug)]
+pub struct PlayersDecision {
+    pub decision: u64,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct RandomNumber {
+    pub random_number: u64,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Failed to get random number.")]
+    FailedToGetRandomNumber,
+
+    #[msg("The decision is invalid.")]
+    InvalidDecision,
+}
+
+
+#[derive(Accounts)]
+pub struct GetRand<'info> {
+    /// CHECK:
+    pub feed_account_1: AccountInfo<'info>,
+    /// CHECK:
+    pub feed_account_2: AccountInfo<'info>,
+    /// CHECK:
+    pub feed_account_3: AccountInfo<'info>,
+    /// CHECK:
+    pub fallback_account: AccountInfo<'info>,
+    #[account(mut)]
+    /// CHECK:
+    pub current_feeds_account: AccountInfo<'info>,
+    #[account(mut)]
+    /// CHECK:
+    pub temp: Signer<'info>,
+
+    /// CHECK:
+    pub rng_program: AccountInfo<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
